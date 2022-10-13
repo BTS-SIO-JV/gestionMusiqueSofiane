1a4c1ebd6001a0b76e7d952435723c6c2d690b67
MeyoGIT <sofiane.bouchi1@gmail.com>
2022-10-13 16:58:09 +0200

    13/10

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Album;
 use App\Repository\AlbumRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,9 +15,13 @@ class AlbumController extends AbstractController
     /**
      * @Route("/albums", name="albums",methods={"GET"})
      */
-    public function listeAlbums(AlbumRepository $repo): Response
+    public function listeAlbums(AlbumRepository $repo, PaginatorInterface $paginator, Request $request): Response
     {
-        $albums=$repo->listeAlbumsComplete(['date'=>'2006'],['nom'=>'asc']);
+        $albums = $paginator->paginate(
+            $repo->listeAlbumsComplete(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            9 /*limit per page*/
+        );
         //$albums=$repo->findBy(['date'=>'2006'],['nom'=>'asc']);
         return $this->render('album/listeAlbums.html.twig', [
             'lesAlbums' => $albums
--- src/Repository/AlbumRepository.php
+++ src/Repository/AlbumRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Album;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Album>
@@ -40,9 +41,9 @@ class AlbumRepository extends ServiceEntityRepository
     }
 
    /**
-    * @return Album[] Returns an array of Album objects
+    * @return Query[] Returns an array of Album objects
     */
-   public function listeAlbumsComplete()
+   public function listeAlbumsComplete(): ?Query
    {
        return $this->createQueryBuilder('a')
             ->select('a','s','art','m')
@@ -51,7 +52,6 @@ class AlbumRepository extends ServiceEntityRepository
             ->innerjoin('a.morceaux','m')
             ->orderBy('a.id', 'ASC')
             ->getQuery()
-            ->getResult()
        ;
    }
 
